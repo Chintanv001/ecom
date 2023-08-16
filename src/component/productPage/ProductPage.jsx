680ae4f4f6e634e522961807d20ace66da852df2
Chintan1812 <chintanvekariyaofficial@gmail.com>
2023-08-16 10:41:43 +0530

    first commit

---
+++ src/component/productPage/ProductPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { data } from '../../data'
+
+const productPage = () => {
+    console.log(data.mobileData.map((data) => data))
+    return (
+        <div></div>
+    )
+}
+
+export default productPage
