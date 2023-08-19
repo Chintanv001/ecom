cef97913ec2a4b3752fb13d438806ddff2cb8f90
Chintan1812 <chintanvekariyaofficial@gmail.com>
2023-08-19 17:01:46 +0530

    Added cart item length

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -3,8 +3,11 @@ import { FaShoppingCart } from 'react-icons/fa';
 import { AiOutlineSearch } from 'react-icons/ai';
 import './Navbar.css'; 
 import { NavLink } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 const Navbar = () => {
+    const cartItems = useSelector(state => state.cartReducer.items)
+    
   return (
     <div className="snavbar">
      
@@ -13,8 +16,8 @@ const Navbar = () => {
         <input type="text" placeholder="Search..." />
       </div>
       <div className="cart">
-      <NavLink to='/cart'><FaShoppingCart /></NavLink>
-        
+      <NavLink to='/cart'><FaShoppingCart/></NavLink>
+      <span className="cart-count">{cartItems.length}</span> 
       </div>
     </div>
   );
